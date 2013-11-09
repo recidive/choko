9b2dbe8fc2e7a4464ed2716cf50e892489cd4112
Henrique Recidive <recidive@gmail.com>
2013-11-09 12:38:15 -0200

    Adding a 'Choko' resource factory service for accessing type data from the frontend.

--- applications/default/public/js/services.js
+++ applications/default/public/js/services.js
@@ -5,6 +5,23 @@ angular.module('choko.services', [])
   // Single value service for Choko version.
   .value('version', '0.0.1')
 
+  .factory('Choko', function($resource) {
+    return $resource('/rest/:type/:key', {
+      type: '@type',
+      key: '@key'
+    },
+    {
+      'get': {
+        method: 'GET',
+        transformResponse: function (data) {
+          return angular.fromJson(data).data;
+        },
+        // Data is an Object, not an Array.
+        isArray: false
+      }
+    });
+  })
+
   // Shared server with application state.
   .factory('applicationState', function($rootScope) {
     var state = {};
