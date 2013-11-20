1bbbdb458dd2ef8b17ff3b62e67399ae6d6bbe81
Henrique Recidive <recidive@gmail.com>
2013-11-20 08:12:30 -0200

    Making possible to start the server on another port than 3000 by using the "-p" argument.

--- server.js
+++ server.js
@@ -4,4 +4,14 @@
 var Server = require('./lib/server');
 
 var server = new Server();
-server.start(3000);
+server.start(getPort());
+
+// Get port from command line argument "-p" or fallback to 3000.
+function getPort() {
+  var port = 3000;
+  var args = process.argv.slice(2);
+  if (args.length && args.shift() === '-p') {
+    port = args.shift() || port;
+  }
+  return port;
+}
