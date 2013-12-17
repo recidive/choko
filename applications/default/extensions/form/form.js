4f1104f81fd614d8d2ececf75c77a7a834227514
Henrique Recidive <recidive@gmail.com>
2013-12-17 00:38:09 -0200

    Adding basic forms for all types that have fields.

--- applications/default/extensions/form/form.js
+++ applications/default/extensions/form/form.js
@@ -13,3 +13,47 @@ form.type = function(types, callback) {
 
   callback(null, newTypes);
 };
+
+/**
+ * The form() hook.
+ */
+form.form = function(forms, callback) {
+  var newForms = {};
+
+  // Create forms for every type on the system except 'type' and 'extension',
+  // the ones that have the 'form' property set to false and the ones that have
+  // no fields.
+  for (var typeName in this.application.types) {
+    var typeSettings = this.application.types[typeName].type.settings;
+    if (typeName != 'type' && typeName != 'extension' && typeSettings.fields) {
+      var form = newForms['type-' + typeName] = {
+        title: typeSettings.title,
+        description: 'Form for the ' + typeSettings.title + ' type.'
+      };
+      // Add fields.
+      form.elements = [];
+      for (var fieldName in typeSettings.fields) {
+        var fieldSettings = typeSettings.fields[fieldName];
+        if (typeof fieldSettings.type === 'string') {
+          form.elements.push({
+            name: fieldName,
+            title: fieldSettings.title,
+            // @todo: don't relate field types with element types directly.
+            type: fieldSettings.type,
+            required: fieldSettings.required || false,
+            weight: fieldSettings.weight || 0,
+          });
+        }
+      }
+      form.elements.push({
+        name: 'submit',
+        title: 'Save',
+        type: 'submit',
+        url: '/rest/' + typeName,
+        weight: 15
+      });
+    }
+  }
+
+  callback(null, newForms);
+};
