835e2f8ed7b28991f2322c6d1ff13cd20a55a351
Lucas Constantino <lucas@taller.net.br>
2014-05-24 16:29:49 -0300

    Refactored app.js to be easier to read and alter.

--- applications/default/public/js/app.js
+++ applications/default/public/js/app.js
@@ -1,7 +1,30 @@
+/*
+ * Main choko application definition file.
+ */
+
 'use strict';
 
-// Declare app level module which depends on services, directives and filters.
-angular.module('choko', ['ngRoute', 'ngResource', 'ngSanitize', 'summernote', 'angularFileUpload', 'choko.services', 'choko.directives', 'choko.filters'])
-.config(['$locationProvider', function($locationProvider) {
-  //$locationProvider.html5Mode(true);
-}]);
+// Define core choko dependencies.
+var dependencies = [
+  'ngRoute',
+  'ngResource',
+  'ngSanitize',
+  'summernote',
+  'angularFileUpload',
+  'choko.services',
+  'choko.directives',
+  'choko.filters'
+];
+
+// Declare main choko module.
+angular.module('choko', dependencies)
+
+  /**
+   * Main configuration.
+   */
+  .config(['$locationProvider', function($locationProvider) {
+
+    // Use HTML5 mode to remove "#" symbols from angular-routed pages.
+    // $locationProvider.html5Mode(true);
+
+  }]);
