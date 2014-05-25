67c838ff19fcd66739a452a0c0eab1b35b5567f4
Lucas Constantino <lucas@taller.net.br>
2014-05-25 00:43:52 -0300

    Updated application version to match the one found on package.json.

--- applications/default/public/js/app.js
+++ applications/default/public/js/app.js
@@ -18,8 +18,10 @@ var dependencies = [
 // Declare main choko module.
 angular.module('choko', dependencies)
 
-  // Define current choko version. 
-  .value('version', '0.0.1')
+  // Define current choko version.
+  // @TODO: we should read package.json and make available not only a version
+  // value but other metadata that might be used thoughout the application.
+  .value('version', '0.0.4')
 
   /**
    * Configures the location provider.
