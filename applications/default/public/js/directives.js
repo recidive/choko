11de1df0aff37bf248a7c1ed7f53c3fe3b2a6283
Lucas Constantino <lucas@taller.net.br>
2014-05-27 00:43:23 -0300

    Issue #113: Refactored directives to dynamically include other directives, making the final implementations simpler.

--- applications/default/public/js/directives.js
+++ applications/default/public/js/directives.js
@@ -17,18 +17,93 @@ angular.module('choko')
   // Directive to replace any tag with overridable templates from the server.
   .directive('ckReplace', ['$http', '$compile', function($http, $compile) {
     return {
-      restrict: 'E',
-      scope: true,
+      restrict: 'EA',
+
+      // As this directive will replace the existing markup, it's better that
+      // we run it previously to most other directives, to avoid dumb processing.
+      priority: 100,
       compile: function(element, attrs) {
+        return function(scope, element, attrs) {
+
+          // Request the template content.
+          var loadTemplate = $http({
+            method: 'GET',
+            // If the directive is an element, "src" should be available.
+            url: attrs.ckReplace || attrs.src,
+            cache: true
+          });
+
+          // When ready, compile the retrieved template.
+          loadTemplate.then(function(result) {
+
+            // Compile the returned template.
+            var compiled = $compile(result.data)(scope);
+
+            // Replace old element with compiled one.
+            element.replaceWith(angular.element(compiled));
+
+          });
+        };
+      }
+    };
+  }])
+
+  // A helper service to handle re-compiling of directives.
+  .factory('ckReplaceAndRecompile', ['$compile', function ($compile) {
+    /**
+     * Creates a new element from the given, copying attributes but removing
+     * the old directive to avoid running it again. Recompiles the new
+     * element and replaces the old with it.
+     */
+    return function (element, directiveToRemove, scope, newTag) {
+
+      // Handle multiple removals using a array of removing directives.
+      var directives  = directiveToRemove.length ? directiveToRemove : [];
+      var tagName     = element.prop('localName');
+      var replacement;
+
+      // Replace the directive, be it a tag name or attribute.
+      directives.forEach(function (directive) {
+        if (tagName == directive) {
+          replacement = angular.element(document.createElement(newTag || 'div'));
+          angular.element.each(element[0].attributes, function (i, attr) {
+            replacement.attr(attr.name, attr.value);
+          });
+          element.replaceWith(replacement);
+          element = replacement;
+        } else {
+          element.removeAttr(directive);
+        }
+      });
+
+      // Recompile the element.
+      $compile(element)(scope || {});
+    }
+  }])
+
+  // Directive to replace form elements with overridable templates from
+  // the server.
+  .directive('ckReplaceElement', ['ckReplaceAndRecompile', function(ckReplaceAndRecompile) {
+    return {
+      restrict: 'EA',
+      // This directive will insert other directives dynamically. We set
+      // priority=999 (high number) to make it run first, but nor before core
+      // Angular directives, as we need their data. (ng-repeat runs at 1000
+      // priority).
+      priority: 999,
+      terminal: true,
+      compile: function (tElement, attrs) {
         return function(scope, element, attrs) {
 
           // Allow for custom templates but fallback to default one based
           // on element type.
           scope.element.template = scope.element.template || 'templates/' + scope.element.type + '.html';
-          
-          // Retrive and compile element template.
-          compileAndReplace(scope, element, scope.element.template);
-        };
+
+          // Append the ck-replace directive.
+          element.attr('ck-replace', scope.element.template);
+
+          ckReplaceAndRecompile(element, ['ck-replace-element', 'ng-repeat'], scope);
+        }
       }
     };
   }])
@@ -36,56 +111,34 @@ angular.module('choko')
   // Handles button or button groups for navigation bars.
   // @todo This directive is specifically used by the navigation extension.
   //       Therefore it should be moved to this extension's directory.
-  .directive('ckButton', ['$http', '$compile', function($http, $compile) {
+  .directive('ckButton', ['ckReplaceAndRecompile', function(ckReplaceAndRecompile) {
     return {
-      restrict: 'E',
+      restrict: 'EA',
       scope: true,
+      // This directive will insert other directives dynamically. We set
+      // priority=999 (high number) to make it run first, but nor before core
+      // Angular directives, as we need their data. (ng-repeat runs at 1000
+      // priority).
+      priority: 999,
+      terminal: true,
       compile: function(element, attrs) {
         return function(scope, element, attrs) {
 
           // Add bootstrap button class.
-          scope.item.classes = scope.item.classes || ['btn-default'];
+          scope.item.classes = scope.item.classes || [];
+          if (scope.item.classes.indexOf('btn-default')+1) {
+            scope.item.classes.push('btn-default');
+          }
 
           // @todo: we should probably allow for custom templates, as we do in
-          // cdReplace directive above.
+          // ckReplaceElement directive above.
           var template = scope.item.items ? '/templates/btn-group-dropdown.html' : '/templates/btn-group-button.html';
           
-          // Retrive and compile element template.
-          compileAndReplace(scope, element, template);
+          // Append the replacement directive.
+          element.attr('ck-replace', template);
+
+          ckReplaceAndRecompile(element, ['ck-button', 'ng-repeat'], scope);
         };
       }
     };
   }]);
-
-/**
- * Helper function to retrive a template for a element, compile it and
- * replace the element with it's newly compiled one.
- * @todo Create a new directive to acomplish this behavior.
- * @param  {object} scope
- * @param  {object} element
- * @param  {string} templateUrl
- */
-function compileAndReplace(scope, element, templateUrl) {
-
-  var $injector = element.injector();
-  var $http     = $injector.get('$http');
-  var $compile  = $injector.get('$compile');
-
-  // Request the template content.
-  var loadTemplate = $http({
-    method: 'GET',
-    url: templateUrl,
-    cache: true
-  });
-
-  // When ready, compile the retrieved template.
-  loadTemplate.then(function(result) {
-
-    // Compile the returned template.
-    var compiled = $compile(result.data)(scope);
-
-    // Replace old element with compiled one.
-    element.replaceWith(angular.element(compiled));
-
-  });
-}
