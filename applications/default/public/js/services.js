74ae63dd7df3d8eb72d1147788f7a5cc709b6b88
Lucas Constantino <lucas@taller.net.br>
2014-07-10 20:56:10 -0300

    Created a provider for parsing methods.

--- applications/default/public/js/services.js
+++ applications/default/public/js/services.js
@@ -33,4 +33,103 @@ angular.module('choko.services', [])
         return state = newState;
       },
     };
+  })
+
+  // Parameter Parser service that can be extend by extensions.
+  .provider('Params', function () {
+
+    // Parser functions work most like Angular filter: they are injectable
+    // functions that return the actual function for parsing. This returned
+    // function will be provided with the param for parsing.
+    var parsers = {};
+
+    /**
+     * Register a new parameter parser.
+     */
+    this.addParser = function (name, parser) {
+      parsers[name] = parser
+    };
+
+    // Regiter the default page param parser.
+    this.addParser('pageParam', function () {
+      return function (param, $scope) {
+        return $scope.page && $scope.page.params && $scope.page.params[param] || null;
+      }
+    });
+
+    // Factory.
+    this.$get = function ($injector) {
+
+      var injectedParsers = {};
+
+      // Construct parsers.
+      Object.keys(parsers).forEach(function (parserName) {
+        injectedParsers[parserName] = $injector.invoke(parsers[parserName]);
+      });
+
+      return {
+
+        /**
+         * Parse a parameter.
+         * @todo: this could make use of promises to provide asyncronous parsing.
+         */
+        parse: function () {
+
+          // Parse arguments to array.
+          var args = [].map.call(arguments, function (arg) {
+            return arg;
+          });
+
+          // Get parsing data.
+          var param = args.shift();
+
+          // Params can be written using pipe strings.
+          if (typeof param == 'string') {
+
+            var pipes = param.split('|'),
+                paramObject = {};
+
+            // Get parameter again.
+            param = pipes.shift();
+
+            // If there is a dynamic param but no pipe action, fill with default.
+            if (!pipes.length && ~param.indexOf(':')) {
+              pipes.push('pageParam');
+            }
+            // If there is no dynamic param nor pipes, param is already parsed.
+            else if (!pipes.length) {
+              return param;
+            }
+
+            // Clean param name from now on.
+            if (~param.indexOf(':')) {
+              param = param.replace(':', '');
+            }
+
+            // Transform into parameter object.
+            pipes.forEach(function (pipe, i) {
+              paramObject[pipe] = i == 0 ? param : null;
+            });
+
+            // Reset param to be the object.
+            param = paramObject;
+          }
+
+          // This will hold parsers returns.
+          var result = null;
+
+          // Execute parsers in order.
+          Object.keys(param).forEach(function (key) {
+            if (!result) {
+              result = param[key];
+            }
+
+            // Parse the value.
+            result = injectedParsers[key].apply(injectedParsers, [result].concat(args));
+          });
+
+          return result;
+        }
+      };
+    };
   });
