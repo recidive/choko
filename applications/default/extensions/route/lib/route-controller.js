5b7d903b997f6bc4aa138db3102366565285ee6c
Lucas Constantino <lucas@taller.net.br>
2014-07-16 15:08:38 -0300

    Minor change to roll back code to what it looked before.

--- applications/default/extensions/route/lib/route-controller.js
+++ applications/default/extensions/route/lib/route-controller.js
@@ -110,9 +110,8 @@ RouteController.prototype.access = function(request, response, callback) {
  * @param {Number} [code] HTTP status code.
  */
 RouteController.respond = function(request, response, content, code) {
-
-  // Set defaults.
-  code = typeof code != 'undefined' ? code : 200;
+  // Default to 200 (success).
+  var code = code || 200;
   var payload = {
     status: {
       code: code
