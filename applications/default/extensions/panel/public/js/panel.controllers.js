0f5cd5c1be29995a7dc51e7d98db6d324149d360
Henrique Recidive <recidive@gmail.com>
2014-10-20 19:07:06 -0200

    Moving PanelController to panel extension.

---
+++ applications/default/extensions/panel/public/js/panel.controllers.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('choko.controllers')
+
+.controller('PanelController', ['$scope', '$controller',
+  function ($scope, $controller) {
+    if ($scope.panel.type && $scope.panel.type !== 'default') {
+      // Set view to the panel itself and call ViewController.
+      $scope.view = $scope.panel;
+
+      // Inherit controller.
+      $controller('ViewController', {
+        $scope: $scope
+      });
+    }
+
+    if ($scope.panel.bare) {
+      $scope.template = $scope.panel.template || '/templates/panel-content.html';
+    }
+    else {
+      $scope.template = '/templates/panel.html';
+    }
+  }]);
