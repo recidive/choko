ee90ce09ab14a09a025b446296b2401fd2c84ae0
Henrique Recidive <recidive@gmail.com>
2014-10-20 19:11:35 -0200

    Moving layout related controllers to layout extension.

---
+++ applications/default/extensions/layout/public/js/layout.controllers.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('choko.controllers')
+
+.controller('RowController', ['$scope',
+  function ($scope) {
+    $scope.name = $scope.row.name;
+
+    $scope.getTemplate = function() {
+      return $scope.template || '/templates/row.html';
+    }
+  }])
+
+.controller('ColumnController', ['$scope',
+  function ($scope) {
+    $scope.name = $scope.column.name;
+
+    $scope.getTemplate = function() {
+      return $scope.template || '/templates/column.html';
+    };
+  }])
+
+.controller('RegionController', ['$scope',
+  function ($scope) {
+
+  }]);
--- applications/default/public/js/controllers.js
+++ applications/default/public/js/controllers.js
@@ -51,29 +51,6 @@ angular.module('choko.controllers', [])
       });
     }])
 
-  .controller('RowController', ['$scope',
-    function ($scope) {
-      $scope.name = $scope.row.name;
-
-      $scope.getTemplate = function() {
-        return $scope.template || '/templates/row.html';
-      }
-    }])
-
-  .controller('ColumnController', ['$scope',
-    function ($scope) {
-      $scope.name = $scope.column.name;
-
-      $scope.getTemplate = function() {
-        return $scope.template || '/templates/column.html';
-      };
-    }])
-
-  .controller('RegionController', ['$scope',
-    function ($scope) {
-
-    }])
-
   .controller('NavigationController', ['$scope', '$location',
     function ($scope, $location) {
 
