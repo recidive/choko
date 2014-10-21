3c57ece01749056455a84ae878a12b3b680692f2
Henrique Recidive <recidive@gmail.com>
2014-10-21 02:41:38 -0200

    Improving displays controllers and templates.

--- applications/default/extensions/display/public/js/display.controllers.js
+++ applications/default/extensions/display/public/js/display.controllers.js
@@ -4,10 +4,16 @@ angular.module('choko.controllers')
 
 .controller('DisplayRegionController', ['$scope',
   function ($scope) {
-
+    if ($scope.display && 'instances' in $scope.display && $scope.view.itemType in $scope.display.instances) {
+      $scope.elements = $scope.display.instances[$scope.view.itemType];
+    }
   }])
 
-.controller('DisplayFieldController', ['$scope',
-  function ($scope) {
-    $scope.field.template = $scope.field.template || '/templates/' + $scope.field.format + '.html';
+.controller('DisplayFieldController', ['$scope', 'Params',
+  function ($scope, Params) {
+    if ($scope.field.fieldName in $scope.item) {
+      $scope.value = $scope.item[$scope.field.fieldName];
+    }
+
+    $scope.template = $scope.field.template || '/templates/' + $scope.field.format + '.html';
   }]);
