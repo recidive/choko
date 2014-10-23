f91fc9cf6b79e4728bcdc61c4ae85dc26bdbb5b5
Henrique Recidive <recidive@gmail.com>
2014-10-22 22:43:00 -0200

    Allow to set element property as string on fields.

--- applications/default/extensions/form/form.js
+++ applications/default/extensions/form/form.js
@@ -129,8 +129,15 @@ form.form = function(forms, callback) {
       }
 
       if (fieldSettings.element) {
-        // Merge in element settings.
-        utils.extend(element, fieldSettings.element);
+        if (typeof fieldSettings.element == 'string') {
+          // Allow a string as element property value in this case this will be
+          // the element type.
+          element.type = fieldSettings.element;
+        }
+        else {
+          // Merge in element settings.
+          utils.extend(element, fieldSettings.element);
+        }
       }
 
       form.elements.push(element);
