8f97f2f4562675ff8e8959c31dce1ca08de801d8
Joseph Ramos <joseph.ramos@taller.net.br>
2014-11-20 12:27:32 -0500

    Updated angular version and dependencies

--- applications/default/public/js/app.js
+++ applications/default/public/js/app.js
@@ -14,5 +14,8 @@ angular.module('choko', [
 ])
 
 .config(['$locationProvider', function($locationProvider) {
-  $locationProvider.html5Mode(true);
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  });
 }]);
